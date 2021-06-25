9f87828dbdbd224f2b873420f16371b92a1b481a
songtoan0302 <nguyensongtoan0302@gmail.com>
2021-06-25 14:04:39 +0700

    update project:spring boot redis cache

--- spring-boot-redis-cache/src/main/java/com/iphayao/cache/model/User.java
+++ spring-boot-redis-cache/src/main/java/com/iphayao/cache/model/User.java
@@ -4,20 +4,20 @@ import lombok.Data;
 import lombok.ToString;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
 
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.io.Serializable;
 @Data
 @ToString
 @Entity
 public class User implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
         private Long id;
+    @Column(name = "firstName")
         private String firstName;
+    @Column(name = "lastName")
         private String lastName;
 
 
